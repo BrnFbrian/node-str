be17eeef7447e39735ca03a08e8c1a864338ac0a
PadreFabioTeMelo <ubicgurwlxhiezsfes@ckptr.com>
2024-02-28 14:14:54 -0300

    update: add funções de create, post e update

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,7 +1,64 @@
 "use strict";
 
 const mongoose = require("mongoose");
-const Product = mongoose.model('Product')
+const Product = mongoose.model("Product");
+
+exports.get = (req, res, next) => {
+  Product.find(
+    {
+      active: true,
+    },
+    "title price slug"
+  )
+    .then((data) => {
+      res.status(200).send({ data });
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+};
+
+exports.getBySlug = (req, res, next) => {
+  Product.findOne(
+    {
+      slug: req.params.slug,
+      active: true,
+    },
+    "title description price slug tags"
+  )
+    .then((data) => {
+      res.status(200).send({ data });
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+};
+
+exports.getById = (req, res, next) => {
+  Product.findById(req.params.id)
+    .then((data) => {
+      res.status(200).send({ data });
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+};
+
+exports.getByTag = (req, res, next) => {
+  Product.find(
+    {
+      tags: req.params.tag,
+      active: true,
+    },
+    "title desscription price slug tags"
+  )
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+};
 
 exports.post = (req, res, next) => {
   var product = new Product(req.body); // O ideal é criar o produto vazio e puxar os elementos que forem necessários
@@ -9,23 +66,36 @@ exports.post = (req, res, next) => {
     .save()
     .then(x => {
       res.status(201).send({
-        message: 'Produto cadastrado com sucesso!'
+        message: "Produto cadastrado com sucesso!",
       });
-    }).catch(e => {
+    })
+    .catch((e) => {
       res.status(400).send({
-        message: 'Falha ao cadastrar o produto',
-        data: e
+        message: "Falha ao cadastrar o produto",
+        data: e,
       });
     });
-
 };
 
 exports.put = (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body,
-  });
+  Product.findByIdAndUpdate(req.params.id, {
+    $set: {
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price
+    }
+  })
+    .then(x => {
+      res.status(200).send({
+        message: "Produto atualizado com sucesso!",
+      });
+    })
+    .catch((e) => {
+      res.status(400).send({
+        message: "Falha ao atualizar o produto.",
+        data: e
+      });
+    });
 };
 
 exports.delete = (req, res, next) => {
