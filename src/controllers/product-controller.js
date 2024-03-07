e65892a9999ecf27b3ff068977699c1a565219eb
PadreFabioTeMelo <ubicgurwlxhiezsfes@ckptr.com>
2024-03-07 10:05:50 -0300

    criação repository

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -2,15 +2,12 @@
 
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
-const ValidationContract = require('../validators/validator');
+const ValidationContract = require("../validators/validator");
+const repository = require("../repositories/product-repository");
 
 exports.get = (req, res, next) => {
-  Product.find(
-    {
-      active: true,
-    },
-    "title price slug"
-  )
+  repository
+    .get()
     .then((data) => {
       res.status(200).send({ data });
     })
@@ -20,13 +17,8 @@ exports.get = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-  Product.findOne(
-    {
-      slug: req.params.slug,
-      active: true,
-    },
-    "title description price slug tags"
-  )
+  repository
+    .getBySlug(req.params.slug)
     .then((data) => {
       res.status(200).send({ data });
     })
@@ -36,7 +28,8 @@ exports.getBySlug = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-  Product.findById(req.params.id)
+  repository
+    .getById(req.params.id)
     .then((data) => {
       res.status(200).send({ data });
     })
@@ -46,13 +39,8 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-  Product.find(
-    {
-      tags: req.params.tag,
-      active: true,
-    },
-    "title desscription price slug tags"
-  )
+  repository
+    .getByTag(req.params.tag)
     .then((data) => {
       res.status(200).send(data);
     })
@@ -63,9 +51,21 @@ exports.getByTag = (req, res, next) => {
 
 exports.post = (req, res, next) => {
   let contract = new ValidationContract();
-  contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteeres');
-  contract.hasMinLen(req.body.slug, 3, 'O título deve conter pelo menos 3 caracteeres');
-  contract.hasMinLen(req.body.description, 3, 'O título deve conter pelo menos 3 caracteeres');
+  contract.hasMinLen(
+    req.body.title,
+    3,
+    "O título deve conter pelo menos 3 caracteeres"
+  );
+  contract.hasMinLen(
+    req.body.slug,
+    3,
+    "O título deve conter pelo menos 3 caracteeres"
+  );
+  contract.hasMinLen(
+    req.body.description,
+    3,
+    "O título deve conter pelo menos 3 caracteeres"
+  );
 
   //se os dados forem invalidos
   if (!contract.isValid()) {
@@ -73,10 +73,9 @@ exports.post = (req, res, next) => {
     return;
   }
 
-  var product = new Product(req.body); // O ideal é criar o produto vazio e puxar os elementos que forem necessários
-  product
-    .save()
-    .then(x => {
+  repository
+    .create(req.body)
+    .then((x) => {
       res.status(201).send({
         message: "Produto cadastrado com sucesso!",
       });
@@ -90,14 +89,9 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-  Product.findByIdAndUpdate(req.params.id, {
-    $set: {
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price
-    }
-  })
-    .then(x => {
+  repository
+    .update(req.params.id, req.body)
+    .then((x) => {
       res.status(200).send({
         message: "Produto atualizado com sucesso!",
       });
@@ -105,23 +99,22 @@ exports.put = (req, res, next) => {
     .catch((e) => {
       res.status(400).send({
         message: "Falha ao atualizar o produto.",
-        data: e
+        data: e,
       });
     });
 };
 
 exports.delete = (req, res, next) => {
-  Product
-    .findOneAndDelete(req.body.id)
-      .then(x => {
+  repository.delete(req.body.id)
+    .then((x) => {
       res.status(200).send({
-        message: 'Produto removido com sucesso!'
+        message: "Produto removido com sucesso!",
       });
     })
     .catch((e) => {
       res.status(400).send({
         message: "Falha ao remover o produto.",
-        data: e
+        data: e,
       });
     });
 };
